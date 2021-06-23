62b10f3a352f9916b98a0065cb5d4f4d20a9b800
joacoramallo <81364341+joacoramallo@users.noreply.github.com>
2021-06-23 20:40:58 -0300

    Add files via upload

---
+++ examen1 jr.sql
@@ -0,0 +1,7 @@
+CREATE DATABASE Examen1;
+CREATE TABLE Estudiantes (
+idEstudiante int  not null primary key,
+Nombre varchar(12) not null,
+Apellido varchar(12) not null,
+Nota int not null,
+Curso varchar(40) not null);
